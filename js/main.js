837fc4e0ffde5b7b08e4e6f24c858066cad33f70
WDRK <lllrrrlrssr@gmail.com>
2021-05-11 16:24:45 +0900

    남은시간 변수를 제어하는 구간 구현함

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 let score = 0;
+let time = 10;
+let isPlaying = false; /* 게임 상태를 관리하는 변수 */
+
 const wordInput = document.querySelector('.word-input');
 const wordDisplay = document.querySelector('.word-display');
 const scoreDisplay = document.querySelector('.score');
+const timeDisplay = document.querySelector('.time');
 
+// 화면의 단어와 사용자가 입력한 단어가 같은지 비교해서 동작하는 구간
 wordInput.addEventListener('input', () => {
   if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
     ++score;
@@ -10,3 +15,10 @@ wordInput.addEventListener('input', () => {
     wordInput.value = '';
   }
 });
+
+// 제한시간 설정
+function countDown() {
+  time > 0 ? time-- : (isPlaying = false);
+  timeDisplay.innerText = time;
+}
+setInterval(countDown, 1000);
