2d6575976842f0dbfafb00c0e45fee005049b546
WDRK <lllrrrlrssr@gmail.com>
2021-05-11 17:10:37 +0900

    초기 구동시 필요한 작업 구현함. 화면에 표시할 단어 목록 생성함

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-const GAME_TIME = 3;
+const GAME_TIME = 3; /* 남은 시간 시작값 */
 let score = 0;
 let time = GAME_TIME;
 let isPlaying = false; /* 게임 상태를 관리하는 변수 */
-let timeInterval;
+let timeInterval; /* interval을 끝낼때 쓰는 변수 */
+let words = []; /* 화면에 표시할 단어 모음 */
 
 const wordInput = document.querySelector('.word-input');
 const wordDisplay = document.querySelector('.word-display');
@@ -10,22 +11,13 @@ const scoreDisplay = document.querySelector('.score');
 const timeDisplay = document.querySelector('.time');
 const button = document.querySelector('.button');
 
-// 화면의 단어와 사용자가 입력한 단어가 같은지 비교해서 동작하는 구간
-wordInput.addEventListener('input', () => {
+// 화면의 단어와 사용자가 입력한 단어가 같은지 비교해서 동작하는 함수
+const checkMatch = () => {
   if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
     ++score;
     scoreDisplay.innerText = score;
     wordInput.value = '';
   }
-});
-
-// 남은시간 설정
-const countDown = () => {
-  time > 0 ? time-- : (isPlaying = false);
-  if (!isPlaying) {
-    clearInterval(timeInterval);
-  }
-  timeDisplay.innerText = time;
 };
 
 const buttonChange = (text) => {
@@ -35,10 +27,29 @@ const buttonChange = (text) => {
     : button.classList.add('loading');
 };
 
+// 화면에 표시할 단어 불러오기
+const getWords = () => {
+  words = ['hello', 'banana', 'apple', 'cherry'];
+  buttonChange('게임시작');
+};
+// 남은시간 설정
+const countDown = () => {
+  time > 0 ? time-- : (isPlaying = false);
+  if (!isPlaying) {
+    clearInterval(timeInterval);
+  }
+  timeDisplay.innerText = time;
+};
+
 const run = () => {
   isPlaying = true;
   time = GAME_TIME;
   timeInterval = setInterval(countDown, 1000);
 };
 
-buttonChange('게임시작');
+// 초기화
+const init = () => {
+  getWords();
+  wordInput.addEventListener('input', checkMatch);
+};
+init();
