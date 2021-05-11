b44b51c95c76a5accf3266227ef4f7063378e140
WDRK <lllrrrlrssr@gmail.com>
2021-05-11 16:16:52 +0900

    main.js에서 화면의 단어와 입력한 단어가 같은지 비교해서 점수를 올리는 구간 구현함

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
+let score = 0;
 const wordInput = document.querySelector('.word-input');
+const wordDisplay = document.querySelector('.word-display');
+const scoreDisplay = document.querySelector('.score');
 
 wordInput.addEventListener('input', () => {
-  console.log(wordInput.value);
+  if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
+    ++score;
+    scoreDisplay.innerText = score;
+    wordInput.value = '';
+  }
 });
