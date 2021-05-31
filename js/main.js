928be4b28e252e8a6b1062af80edb2c4ed1a4f77
WDRK <lllrrrlrssr@gmail.com>
2021-05-31 16:18:30 +0900

    Feat: 추가 현재 상태 감지 checkInterval, axios로 랜덤 단어 가져오기
    프로그램의 현재 진행 상태를 감지하기위해 interval을 추가. 랜덤 단어를 가져오기 위해 axios로 구성

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-const GAME_TIME = 3;
+const GAME_TIME = 7;
 let score = 0;
 let time = GAME_TIME;
 let isPlaying = false;
 let timeInterval;
+let checkInterval;
+let words = [];
 
 const wordDisplay = document.querySelector(".word-display");
 const wordInput = document.querySelector(".word-input");
@@ -10,20 +12,67 @@ const scoreDisplay = document.querySelector(".score");
 const timeDisplay = document.querySelector(".time");
 const button = document.querySelector(".button");
 
-wordInput.addEventListener("input", () => {
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    buttonChange("게임시작");
+    clearInterval(checkInterval);
+  }
+}
+function buttonChange(text) {
+  button.innerText = text;
+  text === "게임시작"
+    ? button.classList.remove("loading")
+    : button.classList.add("loading");
+}
+
+// 단어 불러오기
+function getWords() {
+  axios
+    .get("https://random-word-api.herokuapp.com/word?number=100")
+    .then(function (response) {
+      // handle success
+      response.data.forEach(word => {
+        if (word.length < 10) {
+          words.push(response.data);
+        }
+      });
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+  words = ["hello", "banana", "apple", "cherry"];
+  buttonChange("게임시작");
+}
+
+// 단어 일치 체크
+function checkMatch() {
   if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
+    wordInput.value = "";
+    if (!isPlaying) {
+      return;
+    }
     score++;
     scoreDisplay.innerText = score;
-    wordInput.value = "";
+    time = GAME_TIME;
+    const randomIndex = Math.floor(Math.random() * words.length);
+    wordDisplay.innerText = words[randomIndex];
   }
-});
+}
+init();
 
-buttonChange("게임시작");
-
+// 게임 실행
 function run() {
+  if (isPlaying) {
+    return;
+  }
   isPlaying = true;
   time = GAME_TIME;
+  wordInput.focus();
+  scoreDisplay.innerText = 0;
   timeInterval = setInterval(countDown, 1000);
+  checkInterval = setInterval(checkStatus, 50);
+  buttonChange("게임중");
 }
 
 function countDown() {
@@ -34,9 +83,7 @@ function countDown() {
   timeDisplay.innerText = time;
 }
 
-function buttonChange(text) {
-  button.innerText = text;
-  text === "게임시작"
-    ? button.classList.remove("loading")
-    : button.classList.add("loading");
+function init() {
+  getWords();
+  wordInput.addEventListener("input", checkMatch);
 }
