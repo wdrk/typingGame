cb76c6dede54f697c253378ee0ef39d816d05da2
WDRK <lllrrrlrssr@gmail.com>
2021-05-31 13:21:36 +0900

    Feat: 추가 게임시작 버튼의 클릭 이벤트
    시작버튼을 눌렀을 때 isPlaying 값을 기반으로 게임 주기를 제어하기 위해

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
+const GAME_TIME = 3;
 let score = 0;
-let time = 9;
+let time = GAME_TIME;
 let isPlaying = false;
+let timeInterval;
 
 const wordDisplay = document.querySelector(".word-display");
 const wordInput = document.querySelector(".word-input");
 const scoreDisplay = document.querySelector(".score");
 const timeDisplay = document.querySelector(".time");
+const button = document.querySelector(".button");
 
 wordInput.addEventListener("input", () => {
   if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
@@ -15,9 +18,25 @@ wordInput.addEventListener("input", () => {
   }
 });
 
-setInterval(countDown, 1000);
+buttonChange("게임시작");
+
+function run() {
+  isPlaying = true;
+  time = GAME_TIME;
+  timeInterval = setInterval(countDown, 1000);
+}
 
 function countDown() {
   time > 0 ? time-- : (isPlaying = false);
+  if (!isPlaying) {
+    clearInterval(timeInterval);
+  }
   timeDisplay.innerText = time;
+}
+
+function buttonChange(text) {
+  button.innerText = text;
+  text === "게임시작"
+    ? button.classList.remove("loading")
+    : button.classList.add("loading");
 }
