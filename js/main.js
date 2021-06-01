9a81b4bc05249cc99b943136f9a5d8776e9155c2
WDRK <lllrrrlrssr@gmail.com>
2021-06-01 14:18:42 +0900

    Feat: 추가 함수별 코멘트, init()함수를 즉시 실행 함수로 설정
    프로그램 실행 시 init()함수는 바로 실행해야 하므로

--- js/main.js
+++ js/main.js
@@ -12,17 +12,14 @@ const scoreDisplay = document.querySelector(".score");
 const timeDisplay = document.querySelector(".time");
 const button = document.querySelector(".button");
 
+// 단어를 랜덤으로 선택
 function randomWordPick() {
   const randomIndex = Math.floor(Math.random() * words.length);
   wordDisplay.innerText = words[randomIndex];
 }
 
-function checkStatus() {
-  if (!isPlaying && time === 0) {
-    buttonChange("게임시작");
-    clearInterval(checkInterval);
-  }
-}
+// 버튼의 html class를 조작해서 게임 상태별로 버튼의 CSS를 바꾸기
+// ! input disabled true, false 추가하기
 function buttonChange(text) {
   button.innerText = text;
   if (text === "게임시작") {
@@ -32,7 +29,16 @@ function buttonChange(text) {
   }
 }
 
-// 단어 불러오기
+// 게임 상태를 감지
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    buttonChange("게임시작");
+    clearInterval(checkInterval);
+  }
+}
+
+// 외부 API로 랜덤한 단어 불러오기
+// ! fetch()로 바꿔보기
 function getWords() {
   axios
     .get("https://random-word-api.herokuapp.com/word?number=100")
@@ -65,7 +71,6 @@ function checkMatch() {
     randomWordPick();
   }
 }
-init();
 
 // 게임 실행
 function run() {
@@ -90,8 +95,9 @@ function countDown() {
   timeDisplay.innerText = time;
 }
 
-function init() {
+// 프로그램 실행 시 초기화하는 함수
+(function init() {
   buttonChange("게임로딩중");
   getWords();
   wordInput.addEventListener("input", checkMatch);
-}
+})();
